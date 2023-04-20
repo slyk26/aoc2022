50e1e7740fe98bad8b23ca9ed135104b190a5b6b
slyk26 <slyk26@proton.me>
2023-04-20 23:22:06 +0200

    day5 adjust

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -6,7 +6,7 @@ pub fn part1() {
 }
 
 pub fn part2() {
-   solve_with(crate_mover9001);
+    solve_with(crate_mover9001);
 }
 
 fn solve_with(crate_mover: fn(stacks: &mut Vec<VecDeque<char>>, instructions: &[String]) -> ()) {
@@ -29,14 +29,12 @@ fn get_stacks(crate_part: &[String]) -> Vec<VecDeque<char>> {
     for i in 0..stacksize - 1 {
         let line_as_chars = crate_part.get(i).unwrap().chars().collect::<Vec<char>>();
         let crate_chunks = line_as_chars.chunks(4);
-        let mut stack_counter = 0;
-        for chunk in crate_chunks {
-            if let Some(stack) = stacks.get_mut(stack_counter){
+        for (i, chunk) in crate_chunks.into_iter().enumerate() {
+            if let Some(stack) = stacks.get_mut(i) {
                 if chunk[1] != ' ' {
                     stack.push_front(chunk[1]);
                 }
             }
-            stack_counter += 1;
         };
     }
     stacks
@@ -57,7 +55,7 @@ fn crate_mover9000(stacks: &mut Vec<VecDeque<char>>, instructions: &[String]) {
     })
 }
 
-fn crate_mover9001(stacks: &mut Vec<VecDeque<char>>, instructions: &[String]){
+fn crate_mover9001(stacks: &mut Vec<VecDeque<char>>, instructions: &[String]) {
     instructions.iter().for_each(|instruction| {
         let (amount, from, to) = parse_instruction(&instruction);
         let mut temp: VecDeque<char> = VecDeque::new();
@@ -72,7 +70,7 @@ fn crate_mover9001(stacks: &mut Vec<VecDeque<char>>, instructions: &[String]){
 
 fn parse_instruction(instruction: &String) -> (i32, usize, usize) {
     let words: Vec<&str> = instruction.split(' ').collect();
-    (words[1].parse::<i32>().unwrap(), words[3].parse::<usize>().unwrap() -1, words[5].parse::<usize>().unwrap() -1)
+    (words[1].parse::<i32>().unwrap(), words[3].parse::<usize>().unwrap() - 1, words[5].parse::<usize>().unwrap() - 1)
 }
 
 fn print_solution(stacks: &mut Vec<VecDeque<char>>) {
